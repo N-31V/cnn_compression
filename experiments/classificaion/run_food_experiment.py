4d10ca3d413487b55fc875ea0f1a70e6e7796951
N-31.V <litvintseva.ann@gmail.com>
2023-07-11 13:19:09 +0300

    ImageNet experiment replaced by Food101

--- experiments/classificaion/run_food_experiment.py
+++ experiments/classificaion/run_food_experiment.py
@@ -8,8 +8,8 @@ logging.basicConfig(level=logging.INFO)
 
 print("Starting...")
 start_t = datetime.now()
-task = TASKS['ImageNet']
-# run(task)
+task = TASKS['Food101']
+run(task)
 run(task, mode='svd')
-# run(task, mode='sfp')
+run(task, mode='sfp')
 print(f'Total time: {datetime.now() - start_t}')
